760ec7d8b1232bb985c15fbbab91e71a9fe973f2
Thomas George <private@thomas-george.com>
2022-03-19 12:00:24 +0100

    Initial Commit

---
+++ Meteo-MVVM/Meteo_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meteo_MVVMApp.swift
+//  Meteo-MVVM
+//
+//  Created by Thomas George on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Meteo_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
